b9b6c645322f8fab43b47479b794cdcb26d6df29
tarek <qapooo695@gmail.com>
2024-07-13 18:24:01 +0300

    ch

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,17 +1,33 @@
-#!/usr/bin/node
+#!/usr/bin/env python3
+import requests
+import sys
 
-const request = require('request');
+def get_character_name(url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.json()['name']
+    else:
+        return None
 
-request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, res, body) {
-  if (err) throw err;
-  const actors = JSON.parse(body).characters;
-  exactOrder(actors, 0);
-});
-const exactOrder = (actors, x) => {
-  if (x === actors.length) return;
-  request(actors[x], function (err, res, body) {
-    if (err) throw err;
-    console.log(JSON.parse(body).name);
-    exactOrder(actors, x + 1);
-  });
-};
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: ./0-starwars_characters.js <Movie ID>")
+        sys.exit(1)
+
+    movie_id = sys.argv[1]
+    film_url = f"https://swapi.dev/api/films/{movie_id}/"
+
+    response = requests.get(film_url)
+    if response.status_code != 200:
+        print(f"Error: Unable to fetch movie with ID {movie_id}")
+        sys.exit(1)
+
+    film_data = response.json()
+    characters = film_data['characters']
+
+    for character_url in characters:
+        character_name = get_character_name(character_url)
+        if character_name:
+            print(character_name)
+        else:
+            print(f"Error: Unable to fetch character from {character_url}")
